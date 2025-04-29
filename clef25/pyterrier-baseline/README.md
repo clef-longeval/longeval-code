4f288fa06a6e078e57da7bbde25063754de51fa3
Maik Fröbe <maik.froebe@uni-jena.de>
2025-04-29 16:56:22 +0200

    add tracking to the baseline

--- clef25/pyterrier-baseline/README.md
+++ clef25/pyterrier-baseline/README.md
@@ -4,3 +4,7 @@ ToDo: Describe ...
 ```
 ./baseline.py --dataset longeval-sci/spot-check/* --output-directory output-on-all-spot-checks
 ```
+
+```
+./baseline.py --dataset longeval-sci/spot-check/with-prior-data --output-directory output-on-single-spot-check
+```
--- clef25/pyterrier-baseline/baseline.py
+++ clef25/pyterrier-baseline/baseline.py
@@ -7,6 +7,7 @@ import pandas as pd
 # We use the tracker to monitor resource consumption etc. of the indexing and retrieval.
 # The tracking is optional, i.e., you can remove it or switch to an alternative such as repro_eval.
 from tirex_tracker import tracking
+from shutil import copy
 
 
 def get_index(ir_dataset, index_directory):
@@ -30,14 +31,14 @@ def process_dataset(ir_dataset, output_directory):
         return
 
     index = get_index(ir_dataset, output_directory / "index")
-    with tracking(export_file_path=index_directory / "index-ir-metadata.yml"):
-        bm25 = pt.BatchRetrieve(index, wmodel="BM25")
+    with tracking(export_file_path=output_directory / "retrieval-ir-metadata.yml"):
+        bm25 = pt.terrier.Retriever(index, wmodel="BM25")
 
         # potentially do some query processing
         topics = pd.DataFrame([{'qid': i.query_id, 'query': i.default_text()} for i in ir_dataset.queries_iter()])
 
         # PyTerrier needs to use pre-tokenized queries
-        tokeniser = pt.autoclass("org.terrier.indexing.tokenisation.Tokeniser").getTokeniser()
+        tokeniser = pt.java.autoclass("org.terrier.indexing.tokenisation.Tokeniser").getTokeniser()
 
         topics["query"] = topics["query"].apply(lambda i: ' '.join(tokeniser.getTokens(i)))
 
@@ -56,6 +57,8 @@ def main(dataset, output_directory):
     else:
         for ds in ir_dataset.get_lags():
             process_dataset(ds, Path(output_directory) / ds.get_lag())
+
+    copy(Path(__file__).parent / "ir-metadata.yml", Path(output_directory) / "ir-metadata.yml")
 
 if __name__ == '__main__':
     main()
