653410eff4f2e3365746c77699adda39e552498a
Maik Fröbe <maik.froebe@uni-jena.de>
2025-04-29 22:28:07 +0200

    improve readme

--- clef25/pyterrier-baseline/README.md
+++ clef25/pyterrier-baseline/README.md
@@ -1,10 +1,31 @@
+# PyTerrier Baseline for LongEval'25
 
-ToDo: Describe ...
+This directory contains a PyTerrier baseline for the retrieval task of [LongEval 2025](https://clef-longeval.github.io/). This baseline uses the [LongEval ir_datasets extension](https://github.com/clef-longeval/ir-datasets-longeval) without modification and tracks resource consumption for indexing and retrieval in the ir_metadata format.
+
+## Development
+
+This directory is [configured as DevContainer](https://code.visualstudio.com/docs/devcontainers/containers), i.e., you can open this directory with VS Code or some other DevContainer compatible IDE to work directly in the Docker container with all dependencies installed.
+
+If you want to run it locally, please install the dependencies via `pip3 install -r requirements.txt`.
+
+To create a run, please run:
 
 ```
-./baseline.py --dataset longeval-sci/spot-check/* --output-directory output-on-all-spot-checks
+./baseline.py --dataset longeval-sci/spot-check/* --output output --index indexes
 ```
 
+## Run Submissions to TIRA
+
+To upload your submission to TIRA as a run upload, we assume that you have your valid directory in a directory `OUTPUT_DIRECTORY` for the dataset `DATASET_ID` (see an overview of all dataset ids via [tira.io/datasets?query=longeval-2025](https://archive.tira.io/datasets?query=longeval-2025)).
+
 ```
-./baseline.py --dataset longeval-sci/spot-check/with-prior-data --output-directory output-on-single-spot-check
+tira-cli upload --directory OUTPUT_DIRECTORY --dataset DATASET_ID
+```
+
+## Code Submission to TIRA
+
+As alternative to run submissions above, you can make code submissions where the tira client will build a docker image of your approach from the source code and upload the image to TIRA.io so that your software can run in TIRA.io. To submit this baseline as code submission to TIRA, please run (more detailed information are available in the [documentation](https://docs.tira.io/participants/participate.html#submitting-your-submission):
+
+```
+tira-cli code-submission --path . --task longeval-2025 --dataset sci-spot-check-with-prior-data-20250322-training --command '/baseline.py --dataset $inputDataset --index /tmp/indexes --output $outputDir'
 ```
--- clef25/pyterrier-baseline/baseline.py
+++ clef25/pyterrier-baseline/baseline.py
@@ -7,7 +7,6 @@ import pandas as pd
 import pyterrier as pt
 from ir_datasets_longeval import load
 
-#
 # We use the tracker to monitor resource consumption etc. of the indexing and retrieval.
 # The tracking is optional, i.e., you can remove it or switch to an alternative such as repro_eval.
 from tirex_tracker import tracking
@@ -37,11 +36,11 @@ def get_index(ir_dataset, index_directory):
     return pt.IndexFactory.of(str(index_directory))
 
 
-def process_dataset(ir_dataset, output_directory):
+def process_dataset(ir_dataset, index_directory, output_directory):
     if (output_directory / "run.txt.gz").exists():
         return
 
-    index = get_index(ir_dataset, output_directory / "index")
+    index = get_index(ir_dataset, index_directory)
     with tracking(export_file_path=output_directory / "retrieval-ir-metadata.yml"):
         bm25 = pt.terrier.Retriever(index, wmodel="BM25")
 
@@ -64,26 +63,26 @@ def process_dataset(ir_dataset, output_directory):
 
         run = bm25(topics)
         pt.io.write_results(run, output_directory / "run.txt.gz")
+        copy(index_directory / "index-ir-metadata.yml", output_directory / "index-ir-metadata.yml")
 
 
 @click.command()
 @click.option("--dataset", type=str, help="The dataset id or a local directory.")
-@click.option(
-    "--output-directory", type=click.Path(), required=True, help="The output directory."
-)
-def main(dataset, output_directory):
+@click.option("--output", type=Path, required=True, help="The output directory.")
+@click.option("--index", type=Path, required=True, help="The index directory.")
+def main(dataset, output, index):
     ir_dataset = load(dataset)
 
     if not ir_dataset.get_lags():
-        process_dataset(ir_dataset, Path(output_directory))
+        process_dataset(ir_dataset, index, output)
     else:
         for ds in ir_dataset.get_lags():
-            process_dataset(ds, Path(output_directory) / ds.get_lag())
+            process_dataset(ds, index / ds.get_lag(), output / ds.get_lag())
 
-    copy(
-        Path(__file__).parent / "ir-metadata.yml",
-        Path(output_directory) / "ir-metadata.yml",
-    )
+    # The ir-metadata description of your approach
+    ir_metadata = Path(__file__).parent / "ir-metadata.yml"
+
+    copy(ir_metadata, output / "ir-metadata.yml")
 
 
 if __name__ == "__main__":
