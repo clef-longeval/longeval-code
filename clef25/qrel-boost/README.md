b9beffa917790dfba32aa7b9809add79d94070ec
jueri <jueri.keller@th-koeln.de>
2025-04-30 17:54:35 +0000

    initial commit for qrel boost baseline

---
+++ clef25/qrel-boost/README.md
@@ -0,0 +1,36 @@
+# Qrel Boost Baseline for LongEval'25
+
+This directory contains a Qrel Boost baseline for the retrieval task of [LongEval 2025](https://clef-longeval.github.io/). This baseline uses the [LongEval ir_datasets extension](https://github.com/clef-longeval/ir-datasets-longeval) without modification and tracks resource consumption for indexing and retrieval in the ir_metadata format.
+
+## Development
+
+This directory is [configured as DevContainer](https://code.visualstudio.com/docs/devcontainers/containers), i.e., you can open this directory with VS Code or some other DevContainer compatible IDE to work directly in the Docker container with all dependencies installed.
+
+If you want to run it locally, please install the dependencies via `pip3 install -r requirements.txt`.
+
+To create a run, please run:
+
+```
+./baseline.py --dataset longeval-sci/spot-check/* --output output --index indexes
+```
+
+## Run Submissions to TIRA
+
+To upload your submission to TIRA as a run upload, we assume that you have your valid directory in a directory `OUTPUT_DIRECTORY` for the dataset `DATASET_ID` (see an overview of all dataset ids via [tira.io/datasets?query=longeval-2025](https://archive.tira.io/datasets?query=longeval-2025)).
+
+```
+tira-cli upload --directory OUTPUT_DIRECTORY --dataset DATASET_ID
+```
+
+This will first verify that the content of `OUTPUT_DIRECTORY` is valid and will subsequently upload it, the output should be like:
+
+![Screenshot_20250428_133016](https://github.com/user-attachments/assets/50a4ee85-e599-4c2f-bb83-dcb382d889f4)
+
+
+## Optional: Code Submission to TIRA
+
+As optional alternative to run submissions above, you can make code submissions where the tira client will build a docker image of your approach from the source code and upload the image to TIRA.io so that your software can run in TIRA.io. To submit this baseline as code submission to TIRA, please run (more detailed information are available in the [documentation](https://docs.tira.io/participants/participate.html#submitting-your-submission):
+
+```
+tira-cli code-submission --path . --task longeval-2025 --dataset sci-spot-check-with-prior-data-20250322-training --command '/baseline.py --dataset $inputDataset --index /tmp/indexes --output $outputDir'
+```
---
+++ clef25/qrel-boost/baseline.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+from pathlib import Path
+from shutil import copy
+from typing import Optional
+import click
+import pandas as pd
+import pyterrier as pt
+from ir_datasets_longeval import load
+
+# We use the tracker to monitor resource consumption etc. of the indexing and retrieval.
+# The tracking is optional, i.e., you can remove it or switch to an alternative such as repro_eval.
+from tirex_tracker import tracking
+
+
+
+
+class QrelBoost(pt.Transformer):
+    def __init__(self, dataset, memory: Optional[int] = None):
+        super().__init__()
+        self.dataset = dataset
+        self.memory = memory if memory is not None else len(dataset.prior_datasets)
+
+    def boost(self, doc, _lambda=0.7, mu=2):
+        if doc["label"] == 1:
+            return doc["score"] * _lambda**2
+        elif doc["label"] == 2:
+            return doc["score"] * _lambda**2 * mu
+        else:
+            return doc["score"] * (1 - _lambda) ** 2
+
+    def apply_boost(self, df, prior_dataset):
+        assert prior_dataset != self.dataset.timestamp.strftime(
+            "%Y-%m"
+        ), "Cannot apply boost to the same sub-collection"
+
+        historic_qrels = pt.io.read_qrels(prior_dataset.qrels_path())
+
+        df = df.merge(historic_qrels, on=["docno", "qid"], how="left")
+        df = df.drop_duplicates(keep="first")
+
+        df["score"] = df.apply(self.boost, axis=1)
+        df.drop(columns=["label"], inplace=True)
+        return df
+
+    def transform(self, df):
+        print(f"Applying boost to {self.memory} prior datasets")
+        # df["score"] = df.groupby("qid")["score"].transform(lambda x: x / x.max())
+
+        for prior_dataset in self.dataset.get_past_datasets()[::-1][
+            : self.memory
+        ]:  # reverse to get ascending ordered timestamps and limit to the n last
+            df = self.apply_boost(df, prior_dataset).copy()
+
+        df["rank"] = df.groupby("qid")["score"].rank(ascending=False).astype(int)
+        df = df.sort_values(["qid", "rank"])
+        
+        return df
+    
+
+def get_index(ir_dataset, index_directory):
+    # PyTerrier needs an absolute path
+    index_directory = index_directory.resolve().absolute()
+
+    if (
+        not index_directory.exists()
+        or not (index_directory / "data.properties").exists()
+    ):
+        with tracking(export_file_path=index_directory / "index-ir-metadata.yml"):
+            # build the index
+            indexer = pt.IterDictIndexer(
+                str(index_directory), overwrite=True, meta={"docno": 100, "text": 20480}
+            )
+
+            # you can do some custom document processing here
+            docs = (
+                {"docno": i.doc_id, "text": i.default_text()}
+                for i in ir_dataset.docs_iter()
+            )
+            indexer.index(docs)
+
+    return pt.IndexFactory.of(str(index_directory))
+
+
+def process_dataset(ir_dataset, index_directory, output_directory):
+    if (output_directory / "run.txt.gz").exists():
+        return
+
+    index = get_index(ir_dataset, index_directory)
+    with tracking(export_file_path=output_directory / "retrieval-ir-metadata.yml"):
+        
+        bm25 = pt.terrier.Retriever(index, wmodel="BM25")
+        if ir_dataset.get_past_datasets():
+            print(">>> Using QrelBoost")
+            pipeline = bm25 >> QrelBoost(ir_dataset)
+        else:
+            print(">>> No prior datasets, using BM25")
+            pipeline = bm25
+
+        topics = pd.DataFrame(
+            [
+                {"qid": i.query_id, "query": i.default_text()}
+                for i in ir_dataset.queries_iter()
+            ]
+        )
+
+        # PyTerrier needs to use pre-tokenized queries
+        tokeniser = pt.java.autoclass(
+            "org.terrier.indexing.tokenisation.Tokeniser"
+        ).getTokeniser()
+
+        topics["query"] = topics["query"].apply(
+            lambda i: " ".join(tokeniser.getTokens(i))
+        )
+
+        run = pipeline(topics)
+        pt.io.write_results(run, output_directory / "run.txt.gz")
+        copy(index_directory / "index-ir-metadata.yml", output_directory / "index-ir-metadata.yml")
+
+
+@click.command()
+@click.option("--dataset", type=str, help="The dataset id or a local directory.")
+@click.option("--output", type=Path, required=True, help="The output directory.")
+@click.option("--index", type=Path, required=True, help="The index directory.")
+def main(dataset, output, index):
+    ir_dataset = load(dataset)
+
+    if not ir_dataset.get_lags():
+        process_dataset(ir_dataset, index, output)
+    else:
+        for ds in ir_dataset.get_lags():
+            process_dataset(ds, index / ds.get_lag(), output / ds.get_lag())
+
+    # The ir-metadata description of your approach
+    ir_metadata = Path(__file__).parent / "ir-metadata.yml"
+
+    copy(ir_metadata, output / "ir-metadata.yml")
+
+
+if __name__ == "__main__":
+    main()
