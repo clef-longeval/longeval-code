45620dec6730c5a4c0ab71a63100c0ec1c14a248
Maik Fröbe <maik.froebe@uni-jena.de>
2026-04-28 23:04:57 +0200

    mf

---
+++ clef26/tira-setup/task-3-run-upload/baseline.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+import click
+import json
+
+
+@click.command()
+@click.option("--output", type=Path, required=True)
+def main(output):
+    required = {"snapshot-1": 180, "snapshot-2": 119, "snapshot-3": 182}
+    for k, v in required.items():
+        result = {"meta": {"team_name": "1", "description": "2", "run_name": "3"}}
+        for i in range(v):
+            result[str(k)] = ["a", "b", "c", "d", "e"]
+
+        result = json.dumps(result)
+        (output / (k + ".json")).write_text(result)
+
+
+if __name__ == "__main__":
+    main()
