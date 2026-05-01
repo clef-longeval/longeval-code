838919b9514b6bbaade34ef8ff9d8f0c1500e53a
Maik Fröbe <maik.froebe@uni-jena.de>
2026-05-01 08:03:01 +0200

    mf

--- clef26/tira-setup/task-3-run-upload/baseline.py
+++ clef26/tira-setup/task-3-run-upload/baseline.py
@@ -2,20 +2,49 @@
 from pathlib import Path
 
 import click
+from glob import glob
 import json
+import csv
+
+TEAM_NAME = "my-team"
+DESCRIPTION = "my-description"
+RUN_NAME = "my-run"
+
+
+def load_sessions(file_name):
+    ret = {}
+    with open(file_name) as csvfile:
+        reader = csv.DictReader(csvfile, fieldnames=["index", "user", "session", "query", "timestamp",  "serp", "search-id", "clicks"])
+        for row in reader:
+           if row["search-id"] not in ret:
+               ret[row["search-id"]] = []
+           ret[row["search-id"]].append(row["query"])
+    return ret
+
+
+def make_predictions(sessions):
+    for session in sessions.keys():
+        queries = sessions[session]
+        if len(queries) >= 6:
+            print(queries)
+            raise ValueError("not expected...")
+        while len(queries) < 5:
+            # always predict the last query again
+            queries.append(queries[-1])
+        sessions[session] = queries
+    return sessions
 
 
 @click.command()
+@click.option("--input-dir", type=Path, required=True)
 @click.option("--output", type=Path, required=True)
-def main(output):
-    required = {"snapshot-1": 180, "snapshot-2": 119, "snapshot-3": 182}
-    for k, v in required.items():
-        result = {"meta": {"team_name": "1", "description": "2", "run_name": "3"}}
-        for i in range(v):
-            result[str(i)] = ["a", "b", "c", "d", "e"]
-
-        result = json.dumps(result)
-        (output / (k + ".json")).write_text(result)
+def main(input_dir, output):
+    for i in glob(f"{input_dir}/*.csv"):
+        sessions = load_sessions(f"{input_dir}/{i}")
+        sessions = make_predictions(sessions)
+        sessions["meta"] = {"team_name": TEAM_NAME, "description": DESCRIPTION, "run_name": RUN_NAME}
+        result = json.dumps(sessions)
+        (output/i.split('/')[-1].replace('.csv', '.json')).write_text(result)
 
 
 if __name__ == "__main__":
