58dc337fe764ffee2d8e3f3c98a42488ce1b4d65
Maik Fröbe <maik.froebe@uni-jena.de>
2026-05-12 13:07:38 +0200

    improve task 2 evaluation

--- clef26/topic-extraction/evaluation/evaluate.py
+++ clef26/topic-extraction/evaluation/evaluate.py
@@ -1,62 +1,144 @@
+#!/usr/bin/env python3
 import click
+from ir_datasets_longeval import load
 from pathlib import Path
-import json
-import prompts
-import gzip
-import llm_via_rest_api
 from tqdm import tqdm
+from subprocess import check_output
+from glob import glob
+from trectools import TrecRun, TrecPoolMaker
+import json
+import gzip
+import os
+from llm_predictions import read_failsave, parse_llm_response, run_predictions as run_llm_preds
+
+
+PROMPTS_TO_RUN = ["umbrella_zeroshot_no_desc_no_narrative"]
+
+def all_required_environment_variables_are_set():
+    required_env_to_description = {
+        "OPENAI_API_KEY": "The OPENAI_API_KEY environment variable is required. Please run something like 'export OPENAI_API_KEY=...'",
+        "OPENAI_BASE_URL": "The OPENAI_BASE_URL environment variable is required. Please run something like 'export OPENAI_BASE_URL=https://openrouter.ai/api/v1'",
+        "OPENAI_MODEL": "The OPENAI_MODEL environment variable is required. Please run something like 'export OPENAI_MODEL=mistralai/mistral-small-3.1-24b-instruct:free'",
+    }
+    errors = []
+
+    for k, v in required_env_to_description.items():
+        if k not in os.environ:
+            errors += ["\n\n\t- " + v]
+    
+    if len(errors) > 0:
+        print("Error: The LLM Endpoint is not configured." + ("".join(errors)))
+
+    return len(errors) == 0
+
+
+def load_sub_collections(dataset):
+    ir_dataset = load(dataset)
+    sub_collections = [ir_dataset] if not ir_dataset.get_datasets() else ir_dataset.get_datasets()
+    return {i.get_snapshot(): i for i in sub_collections}
+
+
+def load_all_run_files(dataset, output_directory):
+    if output_directory.is_dir():
+        print(f"Runs already exists. Delete directory to re-download runs: {output_directory}")
+        return
+
+    ir_datasets_id_to_tira_id = {"longeval-sci-2026/clef-2026/sci": "task-1-run-upload-20260428-training"}
+    tira_id = ir_datasets_id_to_tira_id[dataset]
+    cmd = ["tira-cli", "download", "--all-submissions", "--dataset", tira_id, "--output", str(output_directory)]
+    check_output(cmd)
+
+
+def run_pooling(subcollection, output_directory):
+    # TODO remove hard-coded magic number
+    expected_runs = 4
+    runs = glob(f"{output_directory}/retrieval-runs/outputs-flat/*/{subcollection}/*run*")
+    assert len(runs) == expected_runs
+    parsed_runs = [TrecRun(i) for i in tqdm(runs, "Parse runs")]
+    return TrecPoolMaker().make_pool(parsed_runs, strategy="topX", topX=10).pool
+
+
+def pooling_is_already_finished(output_directory, dataset):
+    for i in load_sub_collections(dataset).keys():
+        if not (output_directory / "pooling" / f"{i}.jsonl.gz").is_file():
+            return False
+    return True
+
+def pool_runs(output_directory, dataset):
+    if pooling_is_already_finished(output_directory, dataset):
+        print(f"Pooling already done. Delete directory to re-pool runs: {output_directory/'pooling'}")
+        return
+
+    subcollections = load_sub_collections(dataset)
+    (output_directory / "pooling").mkdir(exist_ok=True, parents=True)
+        
+    for i in subcollections.keys():
+        print(f"Pool {i}")
+        qid_to_query = {q.query_id: q.default_text() for q in subcollections[i].queries_iter()}
+        docs_store = subcollections[i].docs_store()
+        pool = run_pooling(i, output_directory)
+        to_persist = []
+
+        for qid in pool.keys():
+            for docid in pool[qid]:
+                doc = docs_store.get(docid)
+                to_persist.append({
+                    "query_id": qid,
+                    "query": qid_to_query[qid],
+                    "doc_id": docid,
+                    "text": doc.default_text()
+                })
+
+        with gzip.open(output_directory / "pooling" / f"{i}.jsonl.gz", "wt") as f:
+            for l in to_persist:
+                f.write(json.dumps(l) + "\n")
+
+
+def run_llm_predictions(output_directory, dataset):
+    if not all_required_environment_variables_are_set():
+        return
+
+    for i in load_sub_collections(dataset).keys():
+        for prompt in PROMPTS_TO_RUN:
+            target_file = output_directory / 'raw-llm-predictions' / (os.environ["OPENAI_MODEL"].replace("/", "-") + '-' + prompt + '.jsonl.gz')
+            input_file = output_directory / "pooling" / f"{i}.jsonl.gz"
+            run_llm_preds(input_file, prompt, target_file)
+
+
+def create_qrels(output_directory, dataset):
+    if not all_required_environment_variables_are_set():
+        return
+
+    prompt_to_preds = {}
+    for prompt in PROMPTS_TO_RUN:
+        preds_file = output_directory / 'raw-llm-predictions' / (os.environ["OPENAI_MODEL"].replace("/", "-") + '-' + prompt + '.jsonl.gz')
+        preds_parsed = {}
+        for l in read_failsave(preds_file):
+            if l["query_id"] not in preds_parsed:
+                preds_parsed[l["query_id"]] = {}
+            preds_parsed[l["query_id"]][l["doc_id"]] = parse_llm_response(l["prediction"]["content"])[0]
+        prompt_to_preds[prompt] = preds_parsed
+
+    for i in load_sub_collections(dataset).keys():
+        for prompt in PROMPTS_TO_RUN:
+            target = os.environ["OPENAI_MODEL"].replace("/", "-") + '-' + prompt
+            with gzip.open(output_directory / "pooling" / f"{i}.jsonl.gz", "rt") as f, open(output_directory / "qrels" / (target + "-" + i + ".qrels.txt"), "w") as outp:
+                for l in f:
+                    l = json.loads(l)
+                    rel = prompt_to_preds[prompt][l["query_id"]][l["doc_id"]]
+                    outp.write(f"{l['query_id']} 0 {l['doc_id']} {rel}\n")
 
 
 @click.command()
-@click.option('--output-directory', help='The directory where the outputs should be persisted in jsonl format.', type=Path, default=Path('data'))
-@click.option('--prompt', prompt='prompt', help='The prompt to choose.', type=click.Choice(prompts.__all__))
-@click.option('--llm-endpoint', prompt='LLM', help='The llm endpoint')
-@click.option('--model', prompt='model', help='The model to choose.', type=str)
-def run_predictions(output_directory: Path, prompt: str, model: str, llm_endpoint):
-    for dataset in DATASETS:
-        prompt_impl = getattr(prompts, prompt)
-        llm_impl = getattr(llm_via_rest_api, llm)(prompt=prompt_impl, model=model)
+@click.argument('dataset')
+@click.argument('output-directory', type=Path)
+def run_predictions(dataset: str, output_directory: Path):
+    load_all_run_files(dataset, output_directory / "retrieval-runs")
+    pool_runs(output_directory, dataset)
+    run_llm_predictions(output_directory, dataset)
+    create_qrels(output_directory, dataset)
 
-        data_dir = output_directory / dataset.replace('/', '-')
-        target_file = data_dir / 'predictions' / (llm + '-' + model + '-' + prompt + '.jsonl.gz')
-
-        finished_predictions = {}
-
-        with gzip.open(target_file, 'rt') as f:
-            for l in f:
-                try:
-                    l = json.loads(l)
-                    qid, docno = l['query_id'], l['doc_id']
-
-                    if qid not in finished_predictions:
-                        finished_predictions[qid] = set()
-                    finished_predictions[qid].add(docno)
-
-                except json.decoder.JSONDecodeError:
-                    pass
-    
-        to_predict = []
-
-        with gzip.open(data_dir / 'inputs.jsonl.gz', 'rt') as f:
-            for l in f:
-                l = json.loads(l)
-
-                if l['query_id'] in finished_predictions and l['doc_id'] in finished_predictions[l['query_id']]:
-                    continue
-
-                to_predict.append(l)
-    
-        print(f'I must do {len(to_predict)} predictions for {target_file}.')
-
-        if len(to_predict) == 0:
-            continue
-
-        with gzip.open(target_file, 'a') as f:
-            for i in tqdm(to_predict):
-                prediction = llm_impl.generate(query=i['query'], passage=i['text'])
-                to_write = json.dumps({'query_id': i['query_id'], 'doc_id': i['doc_id'], 'prediction': prediction}) + '\n'
-                f.write(to_write.encode('UTF-8'))
-                f.flush()
 
 if __name__ == '__main__':
     run_predictions()
+
--- clef26/topic-extraction/evaluation/prompts.py
+++ clef26/topic-extraction/evaluation/prompts.py
@@ -1,7 +1,21 @@
-from outlines import prompt
+# From https://github.com/castorini/umbrela/blob/main/src/umbrela/prompts/qrel_zeroshot_basic.txt
+def umbrella_zeroshot_no_desc_no_narrative(query: str, document: str):
+    return f"""You are an expert judge of a content. Using your internal knowledge and simple commonsense reasoning, try to verify if the passage is relevance category to the query.
+Here, "0" represent that the passage has nothing to do with the query, "1" represents that the passage has some answer for the query, but the answer may be a bit unclear, or hidden amongst extraneous information and "2" represents that the passage is dedicated to the query and contains the exact answer.
+
+Provide explanation for the relevance and give your answer with from one of the categories 0, 1, or 2 only. One of the categorical values if compulsory in answer.
+
+Instructions: Think about the question. Read the TREC-style query. After explaining your reasoning, provide your answer in terms of 0, 1, or 2 category. Only provide the relevance category on the last line. Do not provide any further details on the last line.
+
+###
+
+Query: """ + query + """
+Document: """ + document + """
+
+Explanation:
+    """
 
 # From https://github.com/castorini/umbrela/blob/main/src/umbrela/prompts/qrel_zeroshot_basic.txt
-@prompt
 def umbrella_zeroshot_basic(query: str, description: str, narrative: str, document: str):
     """You are an expert judge of a content. Using your internal knowledge and simple commonsense reasoning, try to verify if the passage is relevance category to the query.
 Here, "0" represent that the passage has nothing to do with the query, "1" represents that the passage has some answer for the query, but the answer may be a bit unclear, or hidden amongst extraneous information and "2" represents that the passage is dedicated to the query and contains the exact answer.
@@ -19,9 +33,9 @@ Document: {{document}}
 
 Explanation:
     """
+    raise ValueError("sda")
 
 # From https://github.com/castorini/umbrela/blob/main/src/umbrela/prompts/qrel_zeroshot_bing.txt
-@prompt
 def umbrella_zeroshot_bing(query: str, passage: str):
     """Given a query and a passage, you must provide a score on an integer scale of 0 to 2 with the following meanings:
 0 = represent that the passage has nothing to do with the query, 
@@ -44,6 +58,7 @@ Measure how trustworthy the passage is (T).
 Consider the aspects above and the relative importance of each, and decide on a final score (O). Final score must be an integer value only.
 Do not provide any code in result. Provide each score in the format of: ##final score: score without providing any reasoning.
     """
+    raise ValueError("sda")
 
 
 __all__ = sorted(list(['umbrella_zeroshot_basic', 'umbrella_zeroshot_bing']))
